8a0da896e1a25d4f7cb9effc57b0fe55c6ea267d
kon_karin <gd.lsd.168@gmail.com>
2019-07-11 22:44:42 +0900

    コメント削除

--- kon_karin/src/components/parts/Modal.vue
+++ kon_karin/src/components/parts/Modal.vue
@@ -2,7 +2,6 @@
   <div class="modal-mask" @click="$emit('close')">
     <div class="modal-container">
       <img :src="thumbPath" class="modal-img">
-      <!-- <div class="modal-img" :style="{ backgroundImage: `url(${path})` }"></div> -->
     </div>
   </div>
 </template>
