a71151b2eb6081cfa531c9aa3e34f3f39d7f1211
kon_karin <gd.lsd.168@gmail.com>
2019-07-16 17:35:19 +0900

    #6 暫定対応

--- kon_karin/src/components/parts/Modal.vue
+++ kon_karin/src/components/parts/Modal.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="modal-mask" @click="$emit('close')">
     <div class="modal-container">
-      <img :src="thumbPath" class="modal-img">
+      <div v-show="isLoading" class="loader">
+      </div>
+      <transition name="fade-modal">
+        <div v-show="!isLoading">
+          <img @load="loaded" :src="thumbPath" class="modal-img" />
+        </div>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
+
+
 export default {
-  props: ["thumbPath"]
+  props: ["thumbPath"],
+  data() {
+    return {
+      isLoading: true
+    }
+  },
+  methods: {
+    loaded() {
+      this.isLoading = !this.isLoading
+    }
+  }
 };
 </script>
