3218afae78dc23cdd509b456f3a475950bd192b2
kon_karin <gd.lsd.168@gmail.com>
2019-12-19 22:36:58 +0900

    #17 HeaderNavにカレントページの背景色を追加

--- src/components/parts/HeaderNav.vue
+++ src/components/parts/HeaderNav.vue
@@ -3,16 +3,24 @@
     <nav class="header-nav">
       <ul>
         <li>
-          <router-link to="/">Home</router-link>
+          <router-link to="/">
+            Home
+          </router-link>
         </li>
         <li>
-          <router-link to="/about">About</router-link>
+          <router-link to="/about">
+            About
+          </router-link>
         </li>
         <li>
-          <router-link to="/gallery">Gallery</router-link>
+          <router-link to="/gallery">
+            Gallery
+          </router-link>
         </li>
         <li>
-          <router-link to="/contact">Contact</router-link>
+          <router-link to="/contact">
+            Contact
+          </router-link>
         </li>
       </ul>
     </nav>
