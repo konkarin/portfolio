0456f1be202b6b4e145b8bb05c8f8170cb59fba8
kon_karin <gd.lsd.168@gmail.com>
2020-01-23 01:26:57 +0900

    firebaseの警告対応

--- src/components/MyPage.vue
+++ src/components/MyPage.vue
@@ -88,7 +88,10 @@
 </template>
 
 <script>
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
 import loadImage from 'blueimp-load-image'
 
 export default {
