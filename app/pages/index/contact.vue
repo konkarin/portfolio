091bf6cfb2bb2e650fc164588a350bae5de6460d
kobayashi.yosuke <kobayashi.yosuke@cri-mw.co.jp>
2021-02-20 00:51:05 +0900

    update: contactの中央揃えを復元

--- app/pages/index/contact.vue
+++ app/pages/index/contact.vue
@@ -1,18 +1,22 @@
 <template>
   <main class="wrapper">
     <PageTitle>Contact</PageTitle>
-    <div class="textMessage">
-      ご質問、ご意見、お仕事のご依頼等は以下よりお願いいたします。
+    <div class="contact">
+      <div class="contact__message">
+        ご質問、ご意見、お仕事のご依頼等は以下よりお願いいたします。
+      </div>
+      <div class="contact__sns">
+        <a href="https://twitter.com/k0n_karin" target="_blank">
+          <Twitter />
+        </a>
+        <a href="https://www.instagram.com/k0n_karin/" target="_blank">
+          <Instagram />
+        </a>
+        <a href="https://github.com/konkarin" target="_blank">
+          <Github />
+        </a>
+      </div>
     </div>
-    <a href="https://twitter.com/k0n_karin" target="_blank">
-      <Twitter />
-    </a>
-    <a href="https://www.instagram.com/k0n_karin/" target="_blank">
-      <Instagram />
-    </a>
-    <a href="https://github.com/konkarin" target="_blank">
-      <Github />
-    </a>
   </main>
 </template>
 
