b7db72b5355cbfaf445ee6588a401f0211e6f78d
kobayashi.yosuke <kobayashi.yosuke@cri-mw.co.jp>
2021-02-21 13:38:58 +0900

    update: dashboardのページにmetaのtitleを追加

--- app/pages/dashboard/profile.vue
+++ app/pages/dashboard/profile.vue
@@ -1,3 +1,15 @@
 <template>
   <ProfileEditor />
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  head() {
+    return {
+      title: 'Profile',
+    }
+  },
+})
+</script>
