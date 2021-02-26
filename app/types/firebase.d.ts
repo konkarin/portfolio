493c4d012c2d37fbe5929fa8214644fab2fdd323
kobayashi.yosuke <kobayashi.yosuke@cri-mw.co.jp>
2021-02-27 01:26:25 +0900

    update: firebase関連の型定義ファイルを作成

---
+++ app/types/firebase.d.ts
@@ -0,0 +1,11 @@
+import firebase from '@/plugins/firebase'
+
+export type DocumentData = firebase.firestore.DocumentData
+
+export type FirebaseUser = firebase.User
+
+export type OrderByDirection = firebase.firestore.OrderByDirection
+
+export type Timestamp = firebase.firestore.Timestamp
+
+export type FieldValue = firebase.firestore.FieldValue
