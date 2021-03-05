237462098b27973e7f56726ad0ecbfee08e1d2de
kobayashi.yosuke <kobayashi.yosuke@cri-mw.co.jp>
2021-03-06 02:50:27 +0900

    [wip]update: 記事画面の実装

--- app/components/Article/ArticleView.vue
+++ app/components/Article/ArticleView.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="article__container">
     <h1>{{ article.title }}</h1>
-    <div class="article__content">
+    <div class="article__subTitle">{{ updatedDate }} 更新</div>
+    <article class="article__content">
       <!-- eslint-disable-next-line vue/no-v-html -->
       <MarkdownPreview v-html="markdownText" />
+    </article>
+    <div class="article__content articleFooter">
+      <div class="articleFooter__container">
+        <NuxtLink
+          v-for="tag in article.tags"
+          :key="tag"
+          :to="`/articles?tag=${tag}`"
+          class="articleFooter__content articleTag articleTag--link"
+        >
+          {{ tag }}
+        </NuxtLink>
+      </div>
+      <div class="articleFooter__container">
+        <div class="articleFooter__content shareBtn shareBtn--md">
+          <Twitter />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,6 +29,7 @@
 <script lang="ts">
 import Vue, { PropType } from 'vue'
 import { Article } from '@/types/index'
+import Day from '@/utils/day'
 
 export default Vue.extend({
   props: {
@@ -21,6 +40,11 @@ export default Vue.extend({
     markdownText: {
       type: String,
       required: true,
+    },
+  },
+  computed: {
+    updatedDate() {
+      return Day.getDate(this.article.updatedDate, 'YYYY-MM-DD')
     },
   },
 })
