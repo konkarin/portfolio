8c5a91b0096c6c5616c14d62eafe1da3d3a8b92a
kobayashi.yosuke <kobayashi.yosuke@cri-mw.co.jp>
2021-03-13 11:32:08 +0900

    update: 記事更新時にcircleCIのjob実行APIを叩く

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as admin from 'firebase-admin'
 import * as fn from 'firebase-functions'
+import { buildArticles } from './buildArticles'
 import { createUser } from './createUser'
 import { deleteFileFromStorage } from './deleteFileFromStorage'
 import { deleteUser } from './deleteUser'
@@ -20,3 +21,7 @@ exports.deleteFileFromStorage = functions.firestore
 exports.createUser = functions.auth.user().onCreate((user) => createUser(user))
 
 exports.deleteUser = functions.auth.user().onDelete((user) => deleteUser(user))
+
+exports.buildArticles = functions.firestore
+  .document('/users/{uid}/articles/{articleId}')
+  .onWrite(() => buildArticles())
