42568eeec3789a7bff905071c37233f63e331053
kobayashi.yosuke <kobayashi.yosuke@cri-mw.co.jp>
2021-03-20 02:06:56 +0900

    fix: 404及びエラー時の挙動修正

--- app/layouts/error.vue
+++ app/layouts/error.vue
@@ -2,7 +2,7 @@
   <div>
     <HeaderNav />
     <main class="wrapper">
-      <PageTitle>ページが見つかりません</PageTitle>
+      <PageTitle>エラーが発生しました</PageTitle>
     </main>
   </div>
 </template>
@@ -11,7 +11,7 @@
 export default {
   head() {
     return {
-      title: 'Not Found',
+      title: 'Error',
     }
   },
 }
--- app/pages/404.vue
+++ app/pages/404.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>404</div>
+  <div>
+    <HeaderNav />
+    <main class="wrapper">
+      <PageTitle>ページが見つかりません</PageTitle>
+    </main>
+  </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Not Found',
+    }
+  },
+}
+</script>
