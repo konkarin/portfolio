fced19da5ae9f7a38ebf4bb426a27bdebd9e26b0
kobayashi.yosuke <kobayashi.yosuke@cri-mw.co.jp>
2021-03-20 17:45:54 +0900

    update: エラー画面デザイン修正

--- app/layouts/error.vue
+++ app/layouts/error.vue
@@ -3,6 +3,9 @@
     <HeaderNav />
     <main class="wrapper">
       <PageTitle>エラーが発生しました</PageTitle>
+      <div class="error">
+        <NuxtLink to="/" class="btn">トップへ戻る</NuxtLink>
+      </div>
     </main>
   </div>
 </template>
