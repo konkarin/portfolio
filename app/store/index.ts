0886a76d365b8a1c9b15d34aea7e0dfc671b8977
konkarin <konkarin.photo@gmail.com>
2023-08-02 21:21:28 +0900

    refactor: 💡 storeからarticleを削除

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,14 +1,7 @@
-import {
-  getAccessorType,
-  mutationTree,
-  actionTree,
-  getterTree,
-} from 'typed-vuex'
-import { DocumentData } from 'firebase/firestore'
-import { User } from 'firebase/auth'
+import { getAccessorType, mutationTree, actionTree, getterTree } from 'typed-vuex'
+import type { DocumentData } from 'firebase/firestore'
+import type { User } from 'firebase/auth'
 import { db } from '@/api/apis'
-import { Article } from '@/types/index'
-import { Query, Order } from '~/api/firestore'
 
 interface State {
   isAuth: boolean
@@ -20,9 +13,6 @@ interface State {
     show: boolean
     exif: object
   }
-  articles: Article[]
-  recentArticles: Article[]
-  articleTags: string[]
 }
 
 export const state = (): State => ({
@@ -35,9 +25,6 @@ export const state = (): State => ({
     show: false,
     exif: {},
   },
-  articles: [],
-  recentArticles: [],
-  articleTags: [],
 })
 
 export const getters = getterTree(state, {
@@ -64,18 +51,6 @@ export const mutations = mutationTree(state, {
   switchPhotoModal(state: State, payload: State['photoModal']): void {
     state.photoModal = payload
   },
-
-  updateArticles(state: State, payload: Article[]): void {
-    state.articles = payload
-  },
-
-  updateRecentArticles(state: State, payload: Article[]): void {
-    state.recentArticles = payload
-  },
-
-  updateArticleTags(state: State, payload: string[]): void {
-    state.articleTags = payload
-  },
 })
 
 export const actions = actionTree(
@@ -84,24 +59,6 @@ export const actions = actionTree(
     async nuxtServerInit({ commit }): Promise<void> {
       const authorId = this.$config.public.AUTHOR_ID
 
-      // 一覧用の記事一覧
-      const articles = await getArticles(authorId)
-
-      // サイドメニュー用の最新記事一覧
-      const recentArticles = articles.slice(0, 2)
-
-      // サイドメニュー用のタグ一覧
-      const articleTags = await getArticleTags(authorId)
-
-      // 記事が存在しないタグをフィルター
-      const existTags = articleTags.filter((tag) => {
-        return articles.some((article) => article.tags.includes(tag))
-      })
-
-      commit('updateArticles', articles)
-      commit('updateRecentArticles', recentArticles)
-      commit('updateArticleTags', existTags)
-
       // 画像一覧の取得
       const imgList = await getImgList(authorId)
 
@@ -109,36 +66,6 @@ export const actions = actionTree(
     },
   }
 )
-
-const getArticles = async (authorId: string) => {
-  const articlesPath = `users/${authorId}/articles`
-  const queries: Query = {
-    fieldPath: 'isPublished',
-    filterStr: '==',
-    value: true,
-  }
-  const order: Order = {
-    fieldPath: 'releaseDate',
-    direction: 'desc',
-  }
-
-  // 一覧用の記事一覧
-  return (await db
-    .getOrderDocsByQueries(articlesPath, queries, order)
-    .catch((e) => {
-      console.error(e)
-      return []
-    })) as Article[]
-}
-
-const getArticleTags = async (authorId: string) => {
-  const tagsPath = `users/${authorId}/articleTags`
-  // サイドメニュー用のタグ一覧
-  return await db.getDocIds(tagsPath).catch((e) => {
-    console.error(e)
-    return [] as string[]
-  })
-}
 
 const getImgList = async (authorId: string) => {
   const collectionPath = `/users/${authorId}/images`
