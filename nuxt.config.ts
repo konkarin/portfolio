012c3792dbf4acc72a19ed8d29f3caca85a6c77a
konkarin <konkarin.photo@gmail.com>
2023-08-12 23:34:44 +0900

    feat: 🎸 nuxt3導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,45 @@
-import { defineNuxtConfig } from '@nuxt/bridge'
-
 import { generateRoutes } from './routes'
 import { runtimePublicConfig } from './config'
 
 export default defineNuxtConfig({
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  routeRules: {
+    '/': { prerender: true },
+    '/about': { prerender: true },
+    '/gallery': { prerender: true },
+    '/articles': { prerender: true },
+  },
   srcDir: 'app',
-  head: {
-    titleTemplate: "%s - konkarin's photos & blog",
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'og:locale',
-        property: 'og:locale',
-        content: 'ja',
-      },
-      {
-        hid: 'twitter:card',
-        property: 'twitter:card',
-        content: 'summary_large_image',
-      },
-      {
-        hid: 'twitter:creator',
-        property: 'twitter:creator',
-        content: '@k0n_karin',
-      },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  app: {
+    head: {
+      titleTemplate: "%s - konkarin's photos & blog",
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'og:locale',
+          property: 'og:locale',
+          content: 'ja',
+        },
+        {
+          hid: 'twitter:card',
+          property: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          hid: 'twitter:creator',
+          property: 'twitter:creator',
+          content: '@k0n_karin',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
   },
   css: ['@/assets/style/_reset.css', '@/assets/style/style.scss'],
   plugins: [],
@@ -36,20 +49,6 @@ export default defineNuxtConfig({
       pathPrefix: false,
     },
   ],
-  buildModules: ['nuxt-typed-vuex'],
-  router: {
-    extendRoutes(routes: any[]) {
-      routes.push({
-        path: '*',
-        component: './app/layouts/error.vue',
-        props: {
-          error: {
-            message: 'ページが見つかりません',
-          },
-        },
-      })
-    },
-  },
   hooks: {
     async 'nitro:config'(nitroConfig) {
       if (nitroConfig.dev) {
@@ -63,9 +62,6 @@ export default defineNuxtConfig({
         return route.route
       })
     },
-  },
-  server: {
-    port: 3002, // デフォルト: 3000
   },
   runtimeConfig: {
     public: runtimePublicConfig,
