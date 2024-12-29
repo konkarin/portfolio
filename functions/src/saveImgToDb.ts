859d5291e55f6d4fc783de69e1cdda7ebe7dffc8
konkarin <konkarin.photo@gmail.com>
2024-12-29 16:36:24 +0900

    fix: 🐛 ogps以外はサムネ作る

--- functions/src/saveImgToDb.ts
+++ functions/src/saveImgToDb.ts
@@ -21,7 +21,7 @@ export const saveImgToDb = async (object: ObjectMetadata) => {
   const contentType: string = object.contentType
 
   // users配下以外はFirestoreに保存・サムネ作成をしない
-  if (!originalFilePath.includes('ogps')) return
+  if (originalFilePath.includes('ogps')) return
 
   // Exit if this is triggered on a file that is not an image.
   if (!contentType.startsWith('image/')) {
