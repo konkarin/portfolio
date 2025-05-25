fae35d841a3a1ce506533b1ee2073f11c7cb05d3
konkarin <konkarin.photo@gmail.com>
2025-05-25 19:21:01 +0900

    refactor: 💡 styleをコンポーネントに移す

--- app/components/HeaderNav.vue
+++ app/components/HeaderNav.vue
@@ -23,3 +23,45 @@
     </NuxtLink>
   </nav>
 </template>
+
+<style lang="scss" scoped>
+.headerNav {
+  display: flex;
+  justify-content: center;
+  background-color: #fff;
+}
+
+.headerNav__item {
+  position: relative;
+  display: inline-block;
+  color: var(--gray);
+  font-size: 1.2rem;
+  letter-spacing: 0.1em;
+  padding: 1rem 1.5rem;
+  text-decoration: none;
+  transition: 0.3s;
+  &:hover {
+    color: var(--black);
+  }
+}
+
+.headerNav__item--active {
+  color: var(--black);
+}
+
+@media screen and (min-width: $md) {
+  .headerNav__item {
+    transition: color 0.2s;
+    &:hover {
+      color: var(--black);
+      text-decoration: none;
+    }
+  }
+}
+
+@media screen and (max-width: $sm) {
+  .headerNav__item {
+    padding: 0.5rem 1rem;
+  }
+}
+</style>
