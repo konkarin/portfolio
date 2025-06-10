633ad469c2d642dbccbc74a733977db1b9f6d0d8
konkarin <konkarin.photo@gmail.com>
2025-06-11 01:02:28 +0900

    feat: 🎸 ダッシュボードと普通のページをだいたい同じにする

---
+++ app/components/Dashboard/DashboardHeaderNav.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="headerNav">
+    <div class="headerNav__icon"></div>
+    <div>
+      <NuxtLink
+        to="/dashboard/gallery"
+        class="headerNav__item"
+        :class="{ 'headerNav__item--active': $route.path === '/dashboard/gallery' }"
+      >
+        Gallery
+      </NuxtLink>
+      <NuxtLink
+        to="/dashboard/articles"
+        class="headerNav__item"
+        :class="{ 'headerNav__item--active': $route.path.includes('/dashboard/articles') }"
+      >
+        Blog
+      </NuxtLink>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.headerNav {
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+}
+
+.headerNav__item {
+  position: relative;
+  display: inline-block;
+  color: var(--gray);
+  font-size: 1rem;
+  padding: 0 0.6rem;
+  text-decoration: none;
+  transition: 0.3s;
+  &:hover {
+    color: var(--black);
+  }
+}
+
+.headerNav__item--active {
+  color: var(--black);
+}
+
+@media screen and (min-width: $md) {
+  .headerNav__item {
+    transition: color 0.2s;
+    &:hover {
+      color: var(--black);
+      text-decoration: none;
+    }
+  }
+}
+</style>
--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -3,42 +3,22 @@
     <div v-show="isUploading || isLoading" class="overlay">
       <Loader />
     </div>
-    <header v-if="!isLoading" class="auth-area">
-      <template v-if="isAuth">
-        <button class="btn sign-out" @click="_signOut()">Sign out</button>
-      </template>
-      <template v-else>
-        <button class="btn sign-in" @click="signIn()">Sign in</button>
-      </template>
+    <template v-if="isAuth">
+      <header>
+        <DashboardHeaderNav v-if="!$route.params.article" />
+      </header>
+      <main class="dashboard">
+        <NuxtPage />
+      </main>
+    </template>
+    <header v-else class="auth-area">
+      <button class="btn sign-in" @click="signIn()">Sign in</button>
     </header>
-    <main v-if="isAuth" class="dashboard">
-      <nav v-if="!$route.params.article" class="dashboard__nav">
-        <NuxtLink
-          to="/dashboard/articles"
-          class="dashboard__navItem"
-          :class="{
-            'dashboard__navItem--active': $route.path === '/dashboard/articles',
-          }"
-        >
-          Blog
-        </NuxtLink>
-        <NuxtLink
-          to="/dashboard/gallery"
-          class="dashboard__navItem"
-          :class="{
-            'dashboard__navItem--active': $route.path === '/dashboard/gallery',
-          }"
-        >
-          Gallery
-        </NuxtLink>
-      </nav>
-      <NuxtPage />
-    </main>
   </div>
 </template>
 
 <script setup lang="ts">
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from '@firebase/auth'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from '@firebase/auth'
 
 const isLoading = ref(true)
 const isUploading = ref(false)
@@ -62,39 +42,10 @@ const signIn = (): void => {
   const provider = new GoogleAuthProvider()
   signInWithPopup(getAuth(), provider)
 }
-const _signOut = async (): Promise<void> => {
-  isLoading.value = true
-  await signOut(getAuth())
-  isLoading.value = false
-}
-
-const { APP_URL } = useRuntimeConfig().public
-useHead({
-  title: 'Dashboard',
-  meta: [
-    { hid: 'og:type', property: 'og:type', content: 'article' },
-    {
-      hid: 'og:title',
-      property: 'og:title',
-      content: `Dashboard - konkarin's photos & blog`,
-    },
-    {
-      hid: 'og:url',
-      property: 'og:url',
-      content: `${APP_URL}/Dashboard`,
-    },
-    {
-      hid: 'og:image',
-      property: 'og:image',
-      content: 'https://konkarin.photo/HomeImg.jpg',
-    },
-  ],
-})
 </script>
 
 <style lang="scss" scoped>
-.sign-in,
-.sign-out {
+.sign-in {
   margin: 0 0 0 auto;
 }
 
@@ -105,7 +56,8 @@ useHead({
 
 .dashboard {
   max-width: 1200px;
-  margin: 3rem auto 0;
+  margin: 0 auto;
+  padding: 0 4rem;
 
   @media screen and (max-width: $sm) {
     .dashboard {
