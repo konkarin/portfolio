8862d5f4eb5c00046f91542b84c1d97422b1360a
konkarin <konkarin.photo@gmail.com>
2025-08-03 07:26:43 +0000

    refactor: 💡 ページ遷移時にserver componentのstyleがheadから消える問題に対応

--- app/components/HomeBlog.server.vue
+++ app/components/HomeBlog.server.vue
@@ -1,5 +1,5 @@
 <template>
-  <section>
+  <section class="server-component">
     <h2>Blog</h2>
     <ul class="blog">
       <HomeBlogItem v-for="article in recentArticles" :key="article.id" :article />
@@ -15,30 +15,3 @@ const recentArticles = computed(() => {
   return useNuxtApp().$articles.value?.slice(0, 2) || []
 })
 </script>
-
-<style lang="scss" scoped>
-h2 {
-  margin: 1rem 0;
-}
-
-.blog {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-}
-
-.link-button {
-  padding: 0.5rem 1rem;
-  border-radius: 50px;
-  background-color: var(--yellow);
-  font-weight: bold;
-  transition: opacity 0.3s;
-  &:hover {
-    opacity: 0.7;
-  }
-}
-
-.blog-footer {
-  margin-top: 2rem;
-}
-</style>
